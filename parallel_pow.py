017731a5f446b91ea0d74d2cf0aa7d7e2fab40fa
m4rc1o <marcioinaciosantana@gmail.com>
2019-07-04 18:44:24 -0300

    Implementa a busca dinâmica de nounces quando não encontra na faixa inicial

--- parallel_pow.py
+++ parallel_pow.py
@@ -3,15 +3,15 @@ import hashlib
 from mpi4py import MPI
 
 bloco = {
-    "tansacoes": [
-                    "marcio->tarik:1000BTC", 
-                    "tarik->breno:500BTC",
-                    "felipe->marcio:10BTC",
-                    "marcio->tarik:10BTC",
-                    "marcio->marcio:0.5BTC",
+    'tansacoes': [
+                    'marcio->tarik:1000 BTC', 
+                    'tarik->breno:50 BTC',
+                    'felipe->marcio:10 BTC',
+                    'marcio->tarik:10 BTC',
+                    'marcio->marcio:20 BTC',
                  ],
-    "id": 1789,
-    "nounce": 0
+    'id': 1256,
+    'nounce': 0
 }
 
 tempo_ini = MPI.Wtime()
@@ -20,8 +20,9 @@ comm = MPI.COMM_WORLD
 meu_id = comm.Get_rank()
 qtd_processos = comm.Get_size()
 
-qtd_zeros = int(sys.argv[1])
-qtd_testar_total = int(sys.argv[2])
+bloco['id'] = sys.argv[1] #Para produzir hashs aleatórios de blocos com ids passados via bash
+qtd_zeros = int(sys.argv[2])
+qtd_testar_total = int(sys.argv[3])
 
 qtd_testar_local = qtd_testar_total // qtd_processos
 nounce_ini = meu_id*qtd_testar_local
@@ -36,33 +37,48 @@ if meu_id == 0:
 
 encontrado = False
 id_quem_achou = -1
-contador = 1
+contador = 0
 
 while nounce < nounce_fin and not encontrado:
     bloco['nounce'] = nounce
     bloco_hash = hashlib.sha256((str(bloco)).encode()).hexdigest()
     nounce += 1
     
-    #Fazendo o iprobe apenas a cada 1000 iterações, pois ele é demorado
-    if contador%1000 == 0:
-        encontrado = comm.iprobe(source=MPI.ANY_SOURCE, tag=0)
-    
     contador += 1
+    
+    #Fazendo o iprobe apenas a cada 1000 iterações, pois ele é demorado
+    if contador == 1000:
+        encontrado = comm.iprobe(source=MPI.ANY_SOURCE, tag=0)
+        contador = 0
     
     if bloco_hash[0:qtd_zeros] == '0'*qtd_zeros:
         encontrado = True
         id_quem_achou = meu_id
-        
+    
+    #Se o processo finalizou, sem sucesso, os testes na sua faixa de nounces
+    #ele inicia testes em uma nova faixa
+    if nounce == nounce_fin:
+        nounce_ini = nounce_ini + qtd_testar_total
+        nounce = nounce_ini
+        nounce_fin = nounce_ini + qtd_testar_local
+        #print("\nSou o processo", meu_id, "e não encontrei nada na faixa anterior")
+        #print("agora vou verificar de", nounce, "até", nounce_fin)        
 
 if encontrado and meu_id == id_quem_achou:
     print("O processo", meu_id, "encontrou a prova de trabalho")
     print("Prova de trabalho", bloco['nounce'])
     
     tempo_fim = MPI.Wtime()
-    print("Tempo de execução:", tempo_fim - tempo_ini)
     
     #Informando aos outros processos que o hash foi encontrado
     for i in range(qtd_processos):
         comm.isend(encontrado, dest=i, tag=0)
+    
+#Sincronizando para que o tempo possa sempre ser colocado na última
+#posição da lista que será utilizada no colab notebook que executará
+#este código
+comm.Barrier()
+if meu_id == id_quem_achou:
+    print("Tempo de execução:", tempo_fim - tempo_ini)
     
     
